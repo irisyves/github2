834424c17378a4216aa5b00ff722c1ffd9f15cde
irisyves <helenaid37@icloud.com>
2024-11-18 16:07:08 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let myCars=[];
+function setup() {
+  createCanvas(800, 600);
+  for(let  i = 0; i < 100; i++){
+    myCars[i]= new Car(color(255,0,0), random(width), random(height),1,random(360));
+  }
+}
+
+function draw() {
+  background(220);
+  for (let j = 0; j<100; j ++){
+    myCars[j].drive();
+    myCars[j].display();
+    myCars[j].flip();
+  }
+}
